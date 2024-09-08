295483760d90da7ad2e5f60b8cc62083bdb90938
Benard Simon <benardsimon7@gmail.com>
2024-09-08 12:36:05 +0100

    Deleted default react files and added Nav and sideNav files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="text-3xl font-inter font-bold underline">Hello world!</h1>
+      <p className="text-3xl font-inter" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe nobis nesciunt obcaecati eaque in vero corrupti voluptas ullam quos fuga quaerat nam sapiente animi assumenda ex totam dignissimos maiores praesentium placeat sit, pariatur officia! Eos natus sequi nemo impedit quia magni sed sit minima nobis, a soluta vero, maiores, itaque omnis quis doloremque labore exercitationem quod! Reprehenderit asperiores nesciunt delectus ad perferendis eaque, voluptas quaerat architecto ducimus, laudantium voluptates rerum, nam quo blanditiis vel beatae. Molestias sed quas, debitis autem numquam neque voluptas itaque consequatur eum? Culpa repellendus quisquam voluptatum laborum impedit doloribus porro ullam, libero consequuntur nulla ipsum sint!</p>
     </div>
   );
 }
