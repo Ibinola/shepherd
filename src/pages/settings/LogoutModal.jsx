27c1b124979eac491e01421828eb5f5935ad10fe
Bigcedar <godwinekoh@gmail.com>
2024-09-16 02:20:26 +0100

    Added Navigation to Logout

--- src/pages/settings/LogoutModal.jsx
+++ src/pages/settings/LogoutModal.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Calender from "../../assets/svgs/Calendar.svg";
 
-const LogoutModal = ({ showLogoutModal, setShowLogoutModal }) => {
+function LogoutModal({ showLogoutModal, setShowLogoutModal }) {
   const [isVisible, setIsVisible] = useState(showLogoutModal);
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (showLogoutModal) {
@@ -14,6 +16,14 @@ const LogoutModal = ({ showLogoutModal, setShowLogoutModal }) => {
   }, [showLogoutModal]);
 
   if (!isVisible) return null;
+
+  const handleLogout = () => {
+    // Perform logout action
+    console.log("Logged out from all devices");
+
+    // After logging out, navigate to the root path "/"
+    navigate("/");
+  };
 
   return (
     <div className="fixed inset-0 z-30 flex items-center justify-center transition-all duration-300 ease-in-out">
@@ -29,26 +39,24 @@ const LogoutModal = ({ showLogoutModal, setShowLogoutModal }) => {
           className="absolute top-2 right-2 text-gray-600 hover:text-gray-800"
           onClick={() => setShowLogoutModal(false)}
         >
-          <img src={Calender} alt="Close logout modal" />
+          <img src={Calender} alt="" />
         </button>
 
         <div className="text-center border-b p-3">
           <h3 className="text-lg leading-6 text-[#212224] font-[500] text-[14px]">
-            Are you sure you want to log out?
+            Are you sure you want to log out from all devices?
           </h3>
         </div>
 
         <div className="flex text-center justify-center items-center w-[365px] mt-2 max-w-md mx-auto p-4 text-[#212224] font-[400]">
-          You will need to log in again to access your account.
+          This action will log you out from all your active sessions on other
+          devices.
         </div>
 
         <div className="flex justify-center mt-4">
           <button
             className="bg-red-500 text-white py-2 px-4 rounded-md hover:bg-red-600"
-            onClick={() => {
-              // Perform logout action
-              setShowLogoutModal(false);
-            }}
+            onClick={handleLogout}
           >
             Log out
           </button>
@@ -62,6 +70,6 @@ const LogoutModal = ({ showLogoutModal, setShowLogoutModal }) => {
       </div>
     </div>
   );
-};
+}
 
 export default LogoutModal;
